6ad990928314915f342a5afab8c20f7e1feecb08
GledistonFerreira <135056224+glediston@users.noreply.github.com>
2024-10-23 21:22:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 //modulos externos
 
-const inquirer = require("inquirer")
+const inquirer = require("inquire")
 const chalk = require("chalk")
 
 //modulos internos
 
 const fs = require("fs")
+operation()
 
-console.log("Olá Mundo")
+function operation() {
+
+    inquirer
+        .prompt ([
+        {
+        type: "list",
+        name: "action",
+        message: 'O que voçê deseja fazer? ',
+        choices: [
+            'Criar Conta',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair',
+        ],
+    },
+])
+.then()
+.catch((err) => console.log(err))
+}
